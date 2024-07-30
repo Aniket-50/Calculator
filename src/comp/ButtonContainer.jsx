6ac1bd880353f0d7d3c9c508ae6e8eb24ef8739e
Aniket-50 <aniket50pcm@gmail.com>
2024-07-30 11:24:46 +0530

    final

--- src/comp/ButtonContainer.jsx
+++ src/comp/ButtonContainer.jsx
@@ -1,11 +1,53 @@
-import '../css/btn.css'
-function ButtonContainer({onButtonClick}){
-  let ButtonNames=["Ans","C","(",")","1","2","3","+","4","5","6","-","7","8","9","/",".","0","⬅","*"];
+import "../css/btn.css";
+function ButtonContainer({ onButtonClick }) {
+  let ButtonNames = [
+    "Ans",
+    "C",
+    "(",
+    ")",
+    "1",
+    "2",
+    "3",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "7",
+    "8",
+    "9",
+    "/",
+    ".",
+    "0",
+    "⬅",
+    "*",
+  ];
 
-  return <>
-  <div className="container" >
-    {ButtonNames.map(ButtonName=><button key={ButtonName}  onClick={()=> onButtonClick(ButtonName)}>{ButtonName}</button>)}
+  return (
+    <>
+      <div className="container">
+        {ButtonNames.map((ButtonName) => (
+          <button
+            key={ButtonName}
+            className={
+              ButtonName == "/" ||
+              ButtonName == "*" ||
+              ButtonName == "+" ||
+              ButtonName == "-" ||
+              ButtonName == "Ans" ||
+              ButtonName == "C" ||
+              ButtonName == "(" ||
+              ButtonName == ")" 
+                ? "fn"
+                : null
+            }
+            onClick={() => onButtonClick(ButtonName)}
+          >
+            {ButtonName}
+          </button>
+        ))}
       </div>
-  </>
+    </>
+  );
 }
 export default ButtonContainer;
